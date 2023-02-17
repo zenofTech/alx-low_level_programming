97cf203c36d7a35fdc41ca0ef84710fbaec0380b
zenoftech <chosentechnologie@gmail.com>
2023-02-16 17:42:09 -0800

    C program using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - print the string in the put function
+ * main - main block, execution starts here.
  *
- * Program output: \"Programming is like building a multilingual puzzle
  * Return : 0 (Success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - main block, it use printf to print output.
+ *
+ * return: 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

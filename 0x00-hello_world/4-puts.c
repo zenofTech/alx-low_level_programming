7e71ac24f2b9a281edb94a4413b6d15fdb4b7f18
zenoftech <chosentechnologie@gmail.com>
2023-02-16 17:17:43 -0800

    C program that prints a standard Output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/*
+/**
  * main - Runs the whole block of code and gives output
  *
- * Program output is: \"Programming is like building a multilingual puzzle (withoutslash)
- * Returns 0: Success
+ * Program output: \"Programming is like building a multilingual puzzle
+ * Return : 0 (Success)
 */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

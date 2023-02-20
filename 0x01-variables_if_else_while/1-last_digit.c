3dd9a0a3cde3f34f23210fa74592e4e1fa308071
zenoftech <zenoftech.info@gmail.com>
2023-02-19 17:00:40 -0800

    program that prints a-z in lowercase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
- * main - program that prints numbers from 0 to 99.
- * Return: 0
+ * main - main is where the execution begins
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0 (success)
  */
-
 int main(void)
 {
-	int i = 0;
+	int n;
+	int check_last_digit;
 
-	while (i <= 99)
-	{
-		putchar(i / 10 + '0');
-		putchar(i % 10 + '0');
-		if (i != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		i++;
-	}
-putchar('\n');
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	check_last_digit = n % 10;
+
+	if (check_last_digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, check_last_digit);
+	else if (check_last_digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, check_last_digit);
+	else if (check_last_digit < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, check_last_digit);
+
+	return (0);
 }

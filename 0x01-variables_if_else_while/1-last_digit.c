27911d33c8654d65c963a33f0b4acf75fd99889e
zenoftech <zenoftech.info@gmail.com>
2023-02-19 17:15:32 -0800

    rints the alphabet in lowercase, and then in uppercase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 	int n;
-	int check_last_digit;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	check_last_digit = n % 10;
+	last_digit = n % 10;
 
-	if (check_last_digit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, check_last_digit);
-	else if (check_last_digit == 0)
-		printf("Last digit of %i is %i and is 0\n", n, check_last_digit);
-	else if (check_last_digit < 6)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, check_last_digit);
+	if (last_digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
+	else if (last_digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, last_digit);
+	else if (last_digit < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
 
 	return (0);
 }

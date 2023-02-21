e52ffa999ce73d6208d0f1d3c715bfde41cee81e
zenoftech <zenoftech.info@gmail.com>
2023-02-21 15:56:00 -0800

    pushing 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - function to execute the block of code
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c[9] = "_putchar"
+	unsigned int i;
+
+	for (i = 0; i < 8; 1++)
+	{
+		_putchar(c[i]);
+	}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+# 0x02. C - Functions, nested loops
+
+>
+
+- This directory files contain *C* language _Functions, Nested loops_ and it aimed to teach how to use function and creations of *C* header files.

e7b5cbdc0f8f9d8832558967fff8cd2bcb40a370
zenoftech <zenoftech.info@gmail.com>
2023-02-21 18:20:27 -0800

    unction that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
- * print_last_digit - function that computes the absolute value of an integer
- * @c:  is the int that will use for the argument of the function
- * Return: 0 (success
+ * print_last_digit - prints the last digit of a number
+ * @i:  is the int that will use for the argument of the function
+ * Return: integer value
  */
-int print_last_digit(int c)
+
+int print_last_digit(int i)
 {
-	if (c > 0 || c == 0)
-	{
-	_putchar (c % 10 + '0');
-	return (c % 10);
-	}
-	else
-	{
-	c = c * -1;
-	_putchar (c % 10 + '0');
-	return (c % 10);
-	}
+	int last_digit;
+
+	last_digit = i % 10;
+
+	if (last_digit < 0)
+		last_digit = last_digit * -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }

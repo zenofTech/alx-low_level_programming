4d46f0c16bc97b49414b81411b1d2aaf41a8ac21
zenoftech <zenoftech.info@gmail.com>
2023-02-23 13:45:36 -0800

    leap year

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+# include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -9,24 +9,20 @@
  */
 
 int largest_number(int a, int b, int c)
-{
-int largest;
+{int largest;
 
-if (a > b && b > c)
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (b > a && b > c)
+else if (b > c)
 {
 largest = b;
-}
-else if (c > b)
-{
-largest = c;
 }
 else
 {
-largest = b;
+largest = c;
 }
+
 return (largest);
 }

d1d8bcfec93bab81708f7b7f88fe1b7ee8bb6ce7
zenoftech <zenoftech.info@gmail.com>
2023-02-24 15:14:12 -0800

    update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,13 +6,6 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);#ifndef MAIN_H
-#define MAIN_H
+int convert_day(int month, int day);
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day)
-;#endif /* MAIN_H */
+#ifndef MAIN_H /*end MAIN_H*/ 

4aefd6af9fd9fa2be586a2029f34fe2af0d6bfce
zenoftech <zenoftech.info@gmail.com>
2023-02-28 15:40:32 -0800

    a function that prints a string, in reverse

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,10 +4,16 @@
  * _puts - Prints a string .
  * @str: string.
  */
+
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
 	_putchar('\n');
 }

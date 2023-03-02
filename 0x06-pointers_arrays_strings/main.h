831297beda49da948dfc7fd08a7399429f1319c1
zenoftech <zenoftech.info@gmail.com>
2023-03-02 10:07:40 -0800

    addign 0x06 dir

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
+char *string_toupper(char *);
+char *cap_string(char *);
+void print_buffer(char *b, int size);
+void print_number(int n);
+
+#endif 

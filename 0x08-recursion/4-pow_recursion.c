c670eb46bc611b8cd4f778023b97772cc9ce2108
zenoftech <zenoftech.info@gmail.com>
2023-03-08 17:55:04 -0800

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,18 +6,16 @@
  * Return: int
  */
 
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
 
-	if (n < 0)
-	{
+	if (y < 0)
 		return (-1);
-	}
-	else if (n == 0)
-	{
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
 		return (1);
-	}
 
-	return (n * factorial(n - 1));
+	return (x * _pow_recursion(x, y - 1));
 
 }
